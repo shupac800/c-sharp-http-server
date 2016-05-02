43974b21563eca467fc06dc916edc4661d096753
Steve Brownlee <chortlehoort@gmail.com>
2016-05-02 13:23:14 -0500

    Adding simple table creation SQL file that works for SQLite (not MSSQL). Also added the ability to look up a single animal in addition to the list of animals.

--- SimpleServer/AnimalHandler.cs
+++ SimpleServer/AnimalHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
+using Mono.Data.Sqlite;
 
 namespace SimpleServer
 {
@@ -9,43 +11,98 @@ namespace SimpleServer
 		{
 		}
 
-		public string getAllAnimals() {
+		public string getAnimal(string IdAnimal) {
 			string response = "";
 
 			string query = @"
 				SELECT 
-				  a.IdAnimal, 
+				  a.IdAnimal,
 				  a.Name, 
-				  s.Name, 
-				  h.Name
-				FROM Animals a
-				LEFT JOIN Species s ON a.IdSpecies = s.IdSpecies
-				LEFT JOIN Habitats h ON h.IdHabitat = s.IdHabitat
-				";
-			
-			using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Steve\\documents\\visual studio 2015\\Projects\\Invoices\\Invoices\\Invoices.mdf\";Integrated Security=True"))
-			using (SqlCommand cmd = new SqlCommand(query, connection))
+				  h.Name HabitatName,
+				  ht.Name HabitatType,
+				  s.CommonName,
+				  s.ScientificName
+				FROM Animal a
+				INNER JOIN Species s ON a.IdSpecies = s.IdSpecies
+				INNER JOIN Habitat h ON h.IdHabitat = a.IdHabitat
+				INNER JOIN HabitatType ht on ht.IdType = h.IdType
+				WHERE a.IdAnimal = 
+				" + IdAnimal;
+
+			const string connectionString = "URI=file:/Users/stevebrownlee/dev/github/chortlehoort/SimpleServer/SimpleServer/Zoolandia.db";
+
+			System.Data.IDbConnection dbcon = new SqliteConnection(connectionString);
+
+			dbcon.Open();
+			IDbCommand dbcmd = dbcon.CreateCommand();
+			dbcon.CreateCommand ();
+
+			dbcmd.CommandText = query;
+			IDataReader reader = dbcmd.ExecuteReader();
+
+			// Read advances to the next row.
+			while (reader.Read())
 			{
-				connection.Open();
-				using (SqlDataReader reader = cmd.ExecuteReader())
-				{
-					// Check is the reader has any rows at all before starting to read.
-					if (reader.HasRows)
-					{
-						
-						// Read advances to the next row.
-						while (reader.Read())
-						{
-							response += "<div class=\"animal animal-type-"+reader[0]+"\">";
-							response += "<h2>" + reader[1] + "</h2>";
-							response += "<div>"+ reader[2] +"</div>";
-							response += "<div>Lives in the "+ reader[3] +" habitat</div>";
-							response += "</div>";
-						}
-						Console.WriteLine(response);
-					}
-				}
+				response += "<div class=\"animal animal-id-"+reader[0]+"\">";
+				response += "<h2>" + reader[1] + "</h2>";
+				response += "<div>" + reader[5] + "</div>";
+				response += "<div>Lives in the "+ reader[2] +" ("+ reader[3] +" type) habitat</div>";
+				response += "</div>";
 			}
+			Console.WriteLine(response);
+
+			// clean up
+			reader.Dispose();
+			dbcmd.Dispose();
+			dbcon.Close();
+
+			return response;
+		}
+
+
+		public string getAllAnimals() {
+			string response = "";
+
+			const string query = @"
+				SELECT 
+				  a.IdAnimal,
+				  a.Name, 
+				  h.Name HabitatName,
+				  ht.Name HabitatType,
+				  s.CommonName,
+				  s.ScientificName
+				FROM Animal a
+				INNER JOIN Species s ON a.IdSpecies = s.IdSpecies
+				INNER JOIN Habitat h ON h.IdHabitat = a.IdHabitat
+				INNER JOIN HabitatType ht on ht.IdType = h.IdType
+				";
+
+			const string connectionString = "URI=file:/Users/stevebrownlee/dev/github/chortlehoort/SimpleServer/SimpleServer/Zoolandia.db";
+
+			System.Data.IDbConnection dbcon = new SqliteConnection(connectionString);
+
+			dbcon.Open();
+			IDbCommand dbcmd = dbcon.CreateCommand();
+			dbcon.CreateCommand ();
+
+			dbcmd.CommandText = query;
+			IDataReader reader = dbcmd.ExecuteReader();
+
+			// Read advances to the next row.
+			while (reader.Read())
+			{
+				response += "<div class=\"animal animal-id-"+reader[0]+"\">";
+				response += "<h2>" + reader[1] + "</h2>";
+				response += "<div><a href='/animals/"+reader[0]+"'>" + reader[5] + "</a></div>";
+				response += "<div>Lives in the "+ reader[2] +" ("+ reader[3] +" type) habitat</div>";
+				response += "</div>";
+			}
+			Console.WriteLine(response);
+
+			// clean up
+			reader.Dispose();
+			dbcmd.Dispose();
+			dbcon.Close();
 
 			return response;
 		}
